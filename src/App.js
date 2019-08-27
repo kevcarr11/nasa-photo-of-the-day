d117c08cce278f96eceeacad1b7aac16e25c2c0a
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-08-27 18:57:03 -0500

    added api image to page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
+import GetNasaPhoto from "./components/GetNasaPhoto";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
+      
+        <GetNasaPhoto />
+      </div>
   );
 }
 
---
+++ src/components/GetNasaPhoto.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+ const GetNasaPhoto = () => {
+    const [nasaPic, setNasaPic] = useState("");
+
+    useEffect(() => {
+        axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+        .then(res => setNasaPic(res.data.url))
+
+        .catch(err => console.log(err));
+
+    }, []);
+
+    return (
+        <div className="nasaImgContainer">
+            <img className="nasaImg" src={nasaPic} alt="nasa pic of the day" />
+        </div>
+    );
+}
+
+export default GetNasaPhoto;
